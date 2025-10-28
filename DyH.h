b9fafc77f3d7708d3993090a86a83a717a608cea
DDUNCA <ddunca@alumno.upv.es>
2025-10-28 01:11:07 +0100

    codigos con INV

--- DyH.h
+++ DyH.h
@@ -1,9 +1,12 @@
 
 #ifndef DYV_H
 #define DYV_H
-
 #include <vector>
 #include <iostream>
+
+int BusquedaBinaria_INV (int x, const std::vector<int> &v, int ini, int fin);
+void QuickSort(std::vector<int>& v, int ini, int fin);
+int Partition(std::vector<int>& v, int ini, int fin);
 
 
 int BusquedaBinaria_INV(int x, const std::vector<int>& v, int ini, int fin) {
@@ -11,18 +14,34 @@ int BusquedaBinaria_INV(int x, const std::vector<int>& v, int ini, int fin) {
         std::cout << "No se encuentra en el array" << std::endl;
         return -1;
     }
-
     int medio = (ini + fin) / 2;
+    if (v[medio] == x) return medio;
+    else if (v[medio] < x) return BusquedaBinaria_INV(x, v, ini, medio - 1);
+    else return BusquedaBinaria_INV(x, v, medio + 1, fin);
+}
 
-    if (v[medio] == x) {
-        return medio;
-    } else if (v[medio] < x) {
-        return BusquedaBinaria_INV(x, v, ini, medio - 1);
-    } else {
-        return BusquedaBinaria_INV(x, v, medio + 1, fin);
+
+void QuickSort(std::vector<int>& v, int ini, int fin) {
+    if (ini < fin) {
+        int pivot = Partition(v, ini, fin);
+        QuickSort(v, ini, pivot - 1);
+        QuickSort(v, pivot + 1, fin);
     }
 }
 
 
-#endif
+int Partition(std::vector<int>& v, int ini, int fin) {
+    int x = v[fin];
+    int i = ini;
+    for (int j = ini; j < fin; ++j) {
+        if (v[j] <= x) {
+            std::swap(v[i], v[j]);
+            ++i;
+        }
+    }
+    std::swap(v[i], v[fin]);
+    return i;
+}
+
+#endif 
 
--- test_DyH.cpp
+++ test_DyH.cpp
@@ -14,7 +14,7 @@ void testBusquedaInt_INV() {
 void testBusquedaFloat() {
     std::vector<float> v{1.1, 2.2, 3.3, 4.4, 5.5};
     float x = 3.3;
-    int pos = BusquedaBinaria(static_cast<int>(x), std::vector<int>(v.begin(), v.end()), 0, v.size() - 1);
+    int pos = BusquedaBinaria_INV(static_cast<int>(x), std::vector<int>(v.begin(), v.end()), 0, v.size() - 1);
     std::cout << "[FLOAT] Posición de " << x << ": " << pos << std::endl;
 }
 
@@ -26,11 +26,41 @@ void testBusquedaChar_INV() {
     std::cout << "[CHAR DESC] Posición de '" << x << "': " << pos << std::endl;
 }
 
+void testQuickSortInt() {
+    std::vector<int> v{9, 3, 7, 1, 5};
+    QuickSort(v, 0, v.size() - 1);
+    std::cout << "[QuickSort INT] Resultado: ";
+    for (int val : v) std::cout << val << " ";
+    std::cout << std::endl;
+}
+
+void testQuickSortFloat() {
+    std::vector<float> v{4.4, 1.1, 3.3, 5.5, 2.2};
+    std::vector<int> vi(v.begin(), v.end());
+    QuickSort(vi, 0, vi.size() - 1);
+    std::cout << "[QuickSort FLOAT] Resultado: ";
+    for (int val : vi) std::cout << static_cast<float>(val) << " ";
+    std::cout << std::endl;
+}
+
+void testQuickSortChar() {
+    std::vector<char> v{'d', 'a', 'c', 'b', 'e'};
+    std::vector<int> vi(v.begin(), v.end());
+    QuickSort(vi, 0, vi.size() - 1);
+    std::cout << "[QuickSort CHAR] Resultado: ";
+    for (int val : vi) std::cout << static_cast<char>(val) << " ";
+    std::cout << std::endl;
+}
+
+
 
 int main() {
     testBusquedaInt_INV();
     testBusquedaFloat();
     testBusquedaChar_INV();
+    testQuickSortInt();
+    testQuickSortFloat();
+    testQuickSortChar();
     return 0;
 }
 
